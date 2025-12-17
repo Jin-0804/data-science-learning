ad5e69f7a509e9010eef2503a46f5d966d903082
Jin-0804 <jiner080406@gmail.com>
2025-12-18 01:24:14 +0800

    Add calculation for a second list of numbers
    Added functionality to calculate total and average for a second list of numbers.

--- data-science-learning/day1_python_basics.py
+++ data-science-learning/day1_python_basics.py
@@ -11,4 +11,17 @@ average = total / len(numbers)
 print("Total:", total)
 print("Average:", average)
 
+
 # The program calculates the total and average of a list of numbers.
+
+# Upgrade: Try another list
+numbers2 = [5, 15, 25]
+
+total2 = 0
+for n in numbers2:
+    total2 += n
+
+average2 = total2 / len(numbers2)
+
+print("Total2:", total2)
+print("Average2:", average2)
