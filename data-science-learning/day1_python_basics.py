67cd65932caa5eb7c7bd8c72925ffa835a33ed91
Jin-0804 <jiner080406@gmail.com>
2025-12-18 01:14:11 +0800

    Add files via upload

---
+++ data-science-learning/day1_python_basics.py
@@ -0,0 +1,14 @@
+# Day 1 - Python Basics
+
+numbers = [10, 20, 30, 40]
+
+total = 0
+for n in numbers:
+    total += n
+
+average = total / len(numbers)
+
+print("Total:", total)
+print("Average:", average)
+
+# The program calculates the total and average of a list of numbers.
